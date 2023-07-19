c5628a629d11064d1c855b33b2babe11bff89f16
Dineo Jafta <jaftadineo5@gmail.com>
2023-07-19 18:51:20 +0200

    updated the html,css and js files

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
+function setDate(timestamp) {
+  let now = new Date(timestamp);
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
   }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
+  let minute = now.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
   }
   let days = [
     "Sunday",
@@ -17,98 +17,64 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  let day = days[now.getDay()];
+  return `${day} ${hour}:${minute}`;
 }
-
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  return days[day];
-}
-
-function displayForecast(response) {
-  let forecast = response.data.daily;
-
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img
-          src="http://shecodes.io/img/wn/${forecastDay.weather[0].icon}@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
-            forecastDay.temp.max
-          )}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDay.temp.min
-          )}° </span>
-        </div>
-      </div>
-  `;
-    }
-  });
-
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
-
-function getForecast(coordinates) {
-  let apiKey = "b29f2a3361tc3774b40c19e49o4ed6a8";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
-
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
-
-  celsiusTemperature = response.data.temperature.current;
-
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
+function showTemperature(response) {
+  let temperaturePart = document.querySelector("#temp");
+  celsiusPart = response.data.temperature.current;
+  temperaturePart.innerHTML = Math.round(celsiusPart);
+  let cityPart = document.querySelector("#city");
+  cityPart.innerHTML = response.data.city;
+  let conditionPart = document.querySelector("#condition");
+  conditionPart.innerHTML = response.data.condition.description;
+  let humidityPart = document.querySelector("#humii");
+  humidityPart.innerHTML = response.data.temperature.humidity;
+  let windPart = document.querySelector("#speed");
+  windPart.innerHTML = Math.round(response.data.wind.speed);
+  let datePart = document.querySelector("#date");
+  datePart.innerHTML = setDate(response.data.time * 1000);
+  let iconPart = document.querySelector("#icon");
+  iconPart.setAttribute(
     "src",
-    `https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.weather[0].icon}.png`
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
-
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  getForecast(response.data.coord);
+  iconPart.setAttribute("alt", response.data.condition.description);
 }
-
-function search(city) {
+function displaySearch(city) {
   let apiKey = "b29f2a3361tc3774b40c19e49o4ed6a8";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(showTemperature);
 }
-function handleSubmit(event) {
+function searchForCity(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.value);
+  let cityInput = document.querySelector("#city-input");
+  displaySearch(cityInput.value);
 }
+function showFahrenhitTemperature(event) {
+  event.preventDefault();
+  let temperaturePart = document.querySelector("#temp");
+  celsiusTemperature.classList.remove("active");
+  fahrenhitPart.classList.add("active");
+  let fahrenhitTemp = Math.round((celsiusPart * 9) / 5 + 32);
+  temperaturePart.innerHTML = fahrenhitTemp;
+}
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  fahrenhitPart.classList.remove("active");
+  celsiusTemperature.classList.add("active");
+  let temperaturePart = document.querySelector("#temp");
+  temperaturePart.innerHTML = Math.round(celsiusPart);
+}
+let celsiusPart = null;
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
+form.addEventListener("submit", searchForCity);
 
-search("Soweto");
+let fahrenhitPart = document.querySelector("#fahrenhit");
+fahrenhitPart.addEventListener("click", showFahrenhitTemperature);
+
+let celsiusTemperature = document.querySelector("#celsius");
+celsiusTemperature.addEventListener("click", showCelsiusTemperature);
+
+displaySearch("Soweto");
